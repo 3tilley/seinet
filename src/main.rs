9413cb2742a229a259b3352122864c2778ccd465
Max Tilley <1138504+3tilley@users.noreply.github.com>
2024-01-04 23:45:01 +0000

    With keypresses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::thread::sleep;
+use std::time::Duration;
+use crate::driving::input::TerminalInput;
+use crate::driving::input::Input;
+
 mod activation_functions;
 mod neuron;
+mod driving;
 
 fn main() {
     println!("Hello, world!");
+    loop {
+        let input = TerminalInput::get_input();
+        println!("{:?}", input);
+        sleep(Duration::from_millis(60));
+    }
 }
