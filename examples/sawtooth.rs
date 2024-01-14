35bdeef737995937dcc830b0499b60a06da6e60e
Max Tilley <1138504+3tilley@users.noreply.github.com>
2024-01-14 13:16:42 +0000

    Wip

---
+++ examples/sawtooth.rs
@@ -0,0 +1,18 @@
+
+// This isn't quite a sawtooth, it's meant to look something like the below
+// ___/\____
+fn sawtoothish(x: f32) -> f32 {
+    if x < -2.0 {
+        0.0
+    } else if x < 0.0 {
+        2.0 * x + 4.0
+    } else if x < 2.0 {
+        -2.0 * x + 4.0
+    } else {
+        0.0
+    }
+}
+
+fn main() {
+
+}
--- src/neuron.rs
+++ src/neuron.rs
@@ -2,14 +2,15 @@
 pub struct Neuron {
     input_weights: Vec<f32>,
     activation_function: fn(f32) -> f32,
+    bias: f32,
 }
 
 impl Neuron {
-    pub fn pass(&self, input: Vec<f32>) -> f32 {
+    pub fn evaluate_neuron(&self, input: &Vec<f32>) -> f32 {
         assert_eq!(input.len(), self.input_weights.len());
         let mut sum = 0.0;
         for i in 0..input.len() {
-            sum += (self.activation_function)(input[i] * self.input_weights[i])
+            sum += (self.activation_function)(input[i] * self.input_weights[i] + self.bias)
         }
         sum
     }
@@ -17,12 +18,34 @@ impl Neuron {
 
 pub struct Layer {
     neurons: Vec<Neuron>,
+    output: Vec<f32>,
 }
 
 impl Layer {
-
+    pub fn evaluate_layer(&mut self, input: &Vec<f32>) -> &Vec<f32> {
+        for (i, neuron) in self.neurons.iter().enumerate() {
+            self.output[i] = neuron.evaluate_neuron(input);
+        }
+        &self.output
+    }
 }
 
 pub struct Net {
     layers: Vec<Layer>,
+    loss_function: fn(&Vec<f32>) -> f32,
+}
+
+impl Net {
+    pub fn evaluate(&mut self, input: Vec<f32>) -> &Vec<f32> {
+        let mut working_vec = &input;
+        for mut layer in self.layers {
+            working_vec = layer.evaluate_layer(working_vec)
+        }
+        working_vec
+    }
+
+    pub fn evaluate_loss(&mut self, input: Vec<f32>) -> f32 {
+        let predicted = self.evaluate(input);
+        (self.loss_function)(predicted)
+    }
 }
